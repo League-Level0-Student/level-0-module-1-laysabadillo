4783e92c140531354c0732e3a00a501cff671438
codeclub <codeclub@smms-rm23-36.smusd.local>
2018-02-07 14:58:26 -0800

    XGeeks

--- src/XGeeks.java
+++ src/XGeeks.java
@@ -15,10 +15,19 @@ public class XGeeks {
 public static void main(String[] args) {
 
 		// 1. Save the superpower for each person in a variable.
-		String Michelle = JOptionPane.showInputDialog(null,"");
+		String Michelle = "River";
+		String Adrien = "Superspeed";
+		String Shani = "Genarisism";
+		String Facundo = "Sharingan/ Reptication";
+		String Laysa = "Ditto";
+		String Eduardo = "Time Magic";
+		String Andrew = "Teleportation";
+
 		// 2. Ask the user to enter a name. Store their answer in a variable.
+		String input = JOptionPane.showInputDialog(null, "What is your name");
 		
 		// 3. Show the superpower in a pop-up, depending on the name entered. 
+		
 		
 	}
 }
