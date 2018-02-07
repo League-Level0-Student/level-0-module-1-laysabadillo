e0f7f0651727db7231e1a9a360c2e3bb3d04b0fb
codeclub <codeclub@smms-rm23-36.smusd.local>
2018-02-07 14:57:54 -0800

    The Riddler X Geeks (Working)

--- src/TheRiddler.java
+++ src/TheRiddler.java
@@ -1,3 +1,5 @@
+import javax.swing.JOptionPane;
+
 //    Copyright (c) The League of Amazing Programmers 2013-2017
 //    Level 0
 
@@ -8,17 +10,29 @@ public class TheRiddler {
 	public static void main(String[] args) {
 
 		// 1. Make a variable to hold the score
-
+		int score = 0;
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
-
+		String input = JOptionPane.showInputDialog(null, "A man is pushing his car along the road when he comes to a hotel. He shouts, I'm bankrupt! Why?" );
 		// 4. If they got the answer right, pop up "correct!" and increase the score by one
-
+		if(input.equals("He was playing monopoly")){
+			JOptionPane.showMessageDialog(null, "Correct");
+			score++;
+		}
 		// 5. Otherwise, say "wrong" and tell them the answer
-
+			else{ JOptionPane.showMessageDialog(null, "That's Wrong");
 		// 6. Add some more riddles
-
+			String input1 = JOptionPane.showInputDialog(null, "A man is pushing his car along the road when he comes to a hotel. He shouts, I'm bankrupt! Why?" );
+			// 4. If they got the answer right, pop up "correct!" and increase the score by one
+			if(input1.equals("He was playing monopoly")){
+				JOptionPane.showMessageDialog(null, "Correct");
+				score++;
+			}
+			// 5. Otherwise, say "wrong" and tell them the answer
+				else{ JOptionPane.showMessageDialog(null, "That's Wrong");
+			
 		// 2. Make a pop up to show the score.
-		
+		JOptionPane.showMessageDialog(null, score + " Points");
 	}
 }
-
+}	
+}
--- src/XGeeks.java
+++ src/XGeeks.java
@@ -15,11 +15,11 @@ public class XGeeks {
 public static void main(String[] args) {
 
 		// 1. Save the superpower for each person in a variable.
-
+		String Michelle = JOptionPane.showInputDialog(null,"");
 		// 2. Ask the user to enter a name. Store their answer in a variable.
-
+		
 		// 3. Show the superpower in a pop-up, depending on the name entered. 
-
+		
 	}
 }
 
